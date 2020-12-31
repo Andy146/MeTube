2881236b308289bfed741740c29d0d85b89e19be
Anders <anders.engeroen@gmail.com>
2020-12-31 14:29:07 +0100

    Made search function return users that fit the search query as well + search function now returns all videos of a user if that user fits the search

--- lib/search.py
+++ lib/search.py
@@ -1,12 +1,11 @@
 import lib.db_connect as db_lib
+import os
 def search(query):
     conn = db_lib.db_connect()
     cursor = conn.cursor(dictionary=True)
     
     sql_query = "SELECT * FROM video"
     cursor.execute(sql_query)
-
-
     db_return = cursor.fetchall()
 
     sql_query = "SELECT * FROM tags"
@@ -20,6 +19,11 @@ def search(query):
         else:
             break
     query = query.split(' ')
+
+    sql_query = "SELECT user_id, username FROM user"
+    cursor.execute(sql_query)
+    users = cursor.fetchall()
+    user_results = list()
 
     for keyword in query:
         for video in db_return:
@@ -35,19 +39,43 @@ def search(query):
 
                 for dictionary in _temp_videos:
                     results.add(dictionary['video_id'])
-    
+        for user in users:
+            if(keyword.lower() in user['username'].lower() or user['username'].lower() in keyword.lower()):
+                try:
+                    root = os.path.realpath(os.path.dirname("__main__"))
+                    with open(f'{root}/users/{user["user_id"]}/profile.png', 'r') as f:
+                        f.close()
+                    user['img'] = f'/MeTube/users/{user["user_id"]}/profile.png'
+                except FileNotFoundError:
+                    user['img'] = '/MeTube/assets/blank_user.svg'
+                user_results.append(user)
+                sql_query = f"SELECT video_id FROM video WHERE uploader_id={user['user_id']}"
+                cursor.execute(sql_query)
+                _temp_videos = cursor.fetchall()
+
+                for video in _temp_videos:
+                    results.add(video['video_id'])
+
     if(len(results)==1):        #If there is only one result, then you can't create a valid tuple for sql, therefore this is an exception
         results = list(results)
         results = int(results[0])
-        sql = f"SELECT * FROM video WHERE video_id={results}"
+        sql_query = f"SELECT * FROM video WHERE video_id={results}"
     elif(len(results)==0):
-        return list()
+        cursor.close()
+        conn.close()
+        return [user_results]
     else:
         results = tuple(results)
-        sql = f"SELECT * FROM video WHERE video_id IN {results}"
+        sql_query = f"SELECT * FROM video WHERE video_id IN {results}"
         
-    cursor.execute(sql)
+    cursor.execute(sql_query)
 
     results = cursor.fetchall()
+
+
+    cursor.close()
+    conn.close()
+
+    results = [user_results, results]
 
     return results
